bcc5a5e82ffcfc2549fd597c9143fd2f4f39dee3
vihao <tranvihao40@gmail.com>
2024-12-03 20:15:12 +0700

    refactor: change get comment by post key

--- src/hooks/comment/usePostComment.ts
+++ src/hooks/comment/usePostComment.ts
@@ -1,30 +1,31 @@
-import { commentApi } from '@/api/comment'
-import { QueryKeys } from '@/constants/query-keys'
-import { CommentRequest } from '@/models/comment'
-import { AxiosError } from 'axios'
-import { comment } from 'postcss'
-import { Arguments, useSWRConfig } from 'swr'
+import { commentApi } from "@/api/comment";
+import { QueryKeys } from "@/constants/query-keys";
+import { CommentRequest } from "@/models/comment";
+import { AxiosError } from "axios";
+import { comment } from "postcss";
+import { Arguments, mutate, useSWRConfig } from "swr";
 
 export function usePostComment() {
-	const { mutate } = useSWRConfig()
+  // const { mutate } = useSWRConfig()
 
-	async function PostComment(commentData: FormData) {
-		try {
-			const newComment = await commentApi.createComment(commentData)
+  async function PostComment(commentData: FormData) {
+    try {
+      const newComment = await commentApi.createComment(commentData);
 
-			// mutate work list if add successfully
-			mutate(
+      // mutate work list if add successfully
+      /* mutate(
 				(key: Arguments) =>
 				Array.isArray(key) && key.includes(QueryKeys.GET_COMMENT_LIST),
 				undefined,
 				{ revalidate: true }
-			);
+			); */
+      await mutate("get_comment_by_post_id");
 
-			return newComment
-		} catch (error: AxiosError | any) {
-			console.log('Failed to post court:', error);
-		}
-	}
+      return newComment;
+    } catch (error: AxiosError | any) {
+      console.log("Failed to post court:", error);
+    }
+  }
 
-	return PostComment
+  return PostComment;
 }
