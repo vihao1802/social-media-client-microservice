409d45e53334ef7e218f4ccf0b9f2eab729476da
vihao <tranvihao40@gmail.com>
2024-12-04 07:47:29 +0700

    feat: add suggestion friends

--- src/models/relationship.ts
+++ src/models/relationship.ts
@@ -1,3 +1,18 @@
 export interface FollowUserRequest {
   user_id: string;
 }
+
+export interface Recommendation {
+  id: string;
+  userName: string;
+  profile_img: string;
+  mutualFriends: number;
+}
+
+export interface RecommendationPagination {
+  items: Recommendation[];
+  totalItems: number;
+  page: number;
+  pageSize: number;
+  totalPages: number;
+}
