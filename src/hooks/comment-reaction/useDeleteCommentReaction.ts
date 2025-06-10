35f567dc85c913a8183824a05bf9495748f566d5
Huchuynh <tuanhaomach@gmail.com>
2025-06-10 14:09:11 +0700

    feat: update Post and Comment Service (Post Method 2)

--- src/hooks/comment-reaction/useDeleteCommentReaction.ts
+++ src/hooks/comment-reaction/useDeleteCommentReaction.ts
@@ -2,6 +2,9 @@ import { QueryKeys } from '@/constants/query-keys';
 import { commentReactionApi } from '@/api/comment-reaction';
 import { AxiosError } from 'axios';
 import { Arguments, useSWRConfig, SWRConfiguration } from 'swr';
+import { CommentReactionRequest } from '@/models/comment-reaction';
+import { ListResponse } from '@/models/api';
+import { Comment } from '@/models/comment';
 
 export interface UseDeleteCommentReactionProps {
   commentReactionId: string;
@@ -12,17 +15,36 @@ export interface UseDeleteCommentReactionProps {
 export function useDeleteCommentReaction() {
   const { mutate } = useSWRConfig();
 
-  async function DeleteCommentReaction(
-    CommentReactionId: string,
-    commentId: string
-  ) {
+  async function DeleteCommentReaction(request: CommentReactionRequest) {
     try {
-      await commentReactionApi.deleteCommentReaction(CommentReactionId);
+      const res = await commentReactionApi.deleteCommentReaction(request);
 
       // mutate work list if add successfully
-      await mutate([QueryKeys.GET_COMMENT_REACTION, commentId]);
+      await mutate(
+        (key: Arguments) =>
+          Array.isArray(key) &&
+          (key.includes('get_comment_by_post_id') ||
+            key.includes('get_comment_replies')),
+        ((cacheData: ListResponse<Comment> | undefined) => {
+          if (!cacheData || !res) return cacheData;
+          return {
+            ...cacheData,
+            items: cacheData.items.map((comment: Comment) => {
+                      if (comment.id === res.commentId) {
+                        return {
+                          ...comment,
+                          liked: false,
+                          likeCount: comment.likeCount - 1,
+                        };
+                      }
+                      return comment;
+                    }),
+                  };
+                }),
+        { revalidate: false }
+      );
     } catch (error: AxiosError | any) {
-      console.log('Failed to post court:', error);
+      console.log('Failed to delete comment reaction:', error);
     }
   }
 
--- src/hooks/comment-reaction/usePostCommentReaction.ts
+++ src/hooks/comment-reaction/usePostCommentReaction.ts
@@ -3,6 +3,8 @@ import { commentReactionApi } from '@/api/comment-reaction';
 import { AxiosError } from 'axios';
 import { Arguments, useSWRConfig, SWRConfiguration } from 'swr';
 import { CommentReactionRequest } from '@/models/comment-reaction';
+import { ListResponse } from '@/models/api';
+import { Comment } from '@/models/comment';
 
 export interface UsePostCommentReactionProps {
   request: CommentReactionRequest;
@@ -17,7 +19,29 @@ export function usePostCommentReaction() {
     try {
       const newCommentReaction =
         await commentReactionApi.createCommentReaction(request);
-      await mutate([QueryKeys.GET_COMMENT_REACTION, request.commentId]);
+      await mutate(
+        (key: Arguments) =>
+          Array.isArray(key) &&
+          (key.includes('get_comment_by_post_id') ||
+            key.includes('get_comment_replies')),
+        ((cacheData: ListResponse<Comment> | undefined) => {
+          if (!cacheData || !newCommentReaction) return cacheData;
+          return {
+            ...cacheData,
+            items: cacheData.items.map((comment: Comment) => {
+              if (comment.id === newCommentReaction.commentId) {
+                return {
+                  ...comment,
+                  liked: !comment.liked,
+                  likeCount: comment.likeCount + (comment.liked ? -1 : 1),
+                };
+              }
+              return comment;
+            }),
+          };
+        }),
+        { revalidate: false }
+      );
       return newCommentReaction;
     } catch (error: AxiosError | any) {
       console.log('Failed to post comment reaction:', error);
